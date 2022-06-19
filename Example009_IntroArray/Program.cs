2327dc02f04c745c92b0c82cbcaab7662cfe9bf7
Ekaterina Romanenko <ev.romanenko@yandex.com>
2022-06-19 06:58:21 +0300

    IntroArray2

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -8,7 +8,7 @@ int Max(int arg1, int arg2, int arg3)
     return result;
 }
 //
-int[] array = { 11,21,31,41,15,61,17,18,19 };
+int[ ] array = { 11,21,31,41,15,61,17,18,19 };
 
 int max = Max(
     Max(array[0], array[1], array[2]),
